d47cbf721c9c53006744a07b7dda7c335ac4f9eb
tianyanyue <3232546116@qq.com>
2021-01-04 09:56:09 +0800

    merge

---
+++ app/src/main/java/cn/bw/a1805toutiao/TouTiaoApplication.java
@@ -0,0 +1,11 @@
+package cn.bw.a1805toutiao;
+
+import android.app.Application;
+
+public
+class TouTiaoApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
