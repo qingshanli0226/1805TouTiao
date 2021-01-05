c99328b0af19d198bcc6640e34ff288a45f62609
liqingshan <liqingshan@sina.cn>
2021-01-05 14:08:57 +0800

    base

---
+++ app/src/main/java/com/toutiao/bawei/a1805toutiao/MainActivity.java
@@ -0,0 +1,13 @@
+package com.toutiao.bawei.a1805toutiao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
