7015a5fe340bfba1a4cfcc2cc6d9950244182ea9
liqingshan <liqingshan@sina.cn>
2021-01-05 15:15:30 +0800

    add the log

--- app/src/main/java/com/song/a1805toutiao/MainActivity.java
+++ app/src/main/java/com/song/a1805toutiao/MainActivity.java
@@ -1,6 +1,7 @@
 package com.song.a1805toutiao;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("LQS", "add the log");
     }
 
 }
