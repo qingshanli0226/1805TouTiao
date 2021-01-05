fcbca97cab81995fc0114b5b83d473241b0b8dff
Your <you@example.com>
2021-01-05 15:36:13 +0800

    六组打印

--- app/src/main/java/com/toutiao/bawei/a1805toutiao/MainActivity.java
+++ app/src/main/java/com/toutiao/bawei/a1805toutiao/MainActivity.java
@@ -2,6 +2,7 @@ package com.toutiao.bawei.a1805toutiao;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("###","六组打印");
     }
 }
