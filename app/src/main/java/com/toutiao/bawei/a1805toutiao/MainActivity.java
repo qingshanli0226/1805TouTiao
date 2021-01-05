a771a6e5b597243cf09a0b5bf5896e89e12ca934
“yjw” <1285597214@qq.com>
2021-01-05 15:21:27 +0800

    five

--- app/src/main/java/com/toutiao/bawei/a1805toutiao/MainActivity.java
+++ app/src/main/java/com/toutiao/bawei/a1805toutiao/MainActivity.java
@@ -2,6 +2,7 @@ package com.toutiao.bawei.a1805toutiao;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-		for(int i=0;i<10;i++){
-	
+		for(int i=0;i<10;i++){}
+        Log.i("Yoyo", "onCreate: five");
     }
 }
