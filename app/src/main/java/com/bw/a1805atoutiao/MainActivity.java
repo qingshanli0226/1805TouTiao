10108b20e9b087917d20be37979411d9b3dc96d5
123wk <1933730971@qq.com>
2021-01-05 14:03:39 +0800

    二组log测试

---
+++ app/src/main/java/com/bw/a1805atoutiao/MainActivity.java
@@ -0,0 +1,17 @@
+package com.bw.a1805atoutiao;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.e("123","454513153");
+    }
+}
