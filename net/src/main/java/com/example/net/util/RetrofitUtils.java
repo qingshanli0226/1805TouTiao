5e0c34876fb15ab58704fec70af07ac1432946e1
lhj <2450778270@qq.com>
2021-01-06 20:31:44 +0800

    Api

--- net/src/main/java/com/example/net/util/RetrofitUtils.java
+++ net/src/main/java/com/example/net/util/RetrofitUtils.java
@@ -1,9 +1,48 @@
 package com.example.net.util;
 
-<<<<<<< HEAD:net/src/main/java/com/example/net/util/RetrofitUtils.java
-class RetrofitUtils {
-=======
+
+import com.example.net.ShopmallApiService;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public  class RetrofitUtils {
 
->>>>>>> hzw:net/src/main/java/com/example/net/util/MyHttp.java
+    private static volatile ShopmallApiService shopmallApiService;
+
+
+    public static synchronized ShopmallApiService getShopmallApiService(){
+        if (shopmallApiService==null){
+            shopmallApiService = createKSApiService();
+        }
+        return shopmallApiService;
+    }
+
+    public static ShopmallApiService createKSApiService(){
+
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient.Builder builder = new OkHttpClient.Builder()
+                .addInterceptor(httpLoggingInterceptor)
+                .writeTimeout(5, TimeUnit.MINUTES)
+                .readTimeout(5, TimeUnit.MINUTES)
+                .callTimeout(5, TimeUnit.MINUTES);
+
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://toutiao.com/")
+                .client(builder.build())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        return retrofit.create(ShopmallApiService.class);
+    }
+
+
 }
