b5527d674d01aa711cd4781e41d62655e7a911d4
严东明 <2328934408@qq.com>
2021-01-06 08:32:33 +0800

    添加网络框架

---
+++ net/src/main/java/com/bw/net/Contents.java
@@ -0,0 +1,6 @@
+package com.bw.net;
+
+public class Contents {
+
+    public static final String BASE_URL = "";
+}
---
+++ net/src/main/java/com/bw/net/INetWorkApiService.java
@@ -0,0 +1,7 @@
+package com.bw.net;
+
+public interface INetWorkApiService {
+
+
+
+}
