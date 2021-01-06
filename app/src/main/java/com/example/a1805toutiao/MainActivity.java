64b8014b3f57dcb9ca172cc931e7371f917c2ead
Your <you@example.com>
2021-01-06 09:07:05 +0800

    改变

--- app/src/main/java/com/example/a1805toutiao/MainActivity.java
+++ app/src/main/java/com/example/a1805toutiao/MainActivity.java
@@ -11,8 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("###","11111111");
-        Log.d("###","122335663332");
+        Log.e("###","adsfaffafdasf");
+        Log.d("###","fasdfsfsdf");
 
 
     }
