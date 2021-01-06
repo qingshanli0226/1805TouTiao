298e47fa5a8e9d884470fb4e19df4f7f0fc04d9d
DELL <Zhu>
2021-01-06 14:03:20 +0800

    123

--- app/src/main/java/com/example/onemyapp/MainActivity.java
+++ app/src/main/java/com/example/onemyapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        
+        Log.i("---","111");
     }
 }
