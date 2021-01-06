ebdbb8b024df0c87385ba153d0da7bd3d9b81e64
123wk <1933730971@qq.com>
2021-01-06 19:03:39 +0800

    初始

--- app/src/main/java/com/bw/a1805atoutiao/MainActivity.java
+++ app/src/main/java/com/bw/a1805atoutiao/MainActivity.java
@@ -1,7 +1,6 @@
 package com.bw.a1805atoutiao;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,14 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.e("two","454513git");
-        Log.e("zgh","123456");
-        Log.e("wk","1235");
-        Log.e("123","sdsad");
-        Log.e("123","sdsad");
-        Log.e("123","sdsad");
-        Log.e("123","sdsad");
-        Log.e("123","sdsad");
+
 
     }
 }
--- user/src/main/java/com/bw/user/Regist.java
+++
@@ -1,4 +0,0 @@
-package com.bw.user;
-
-class Regist {
-}
