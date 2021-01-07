1fe03cbb0e96aae0e18377477c3b27bb49558d63
“yjw” <1285597214@qq.com>
2021-01-07 20:33:47 +0800

    协调者布局,底部导航

--- app/src/main/java/com/example/a1805toutiao/MainActivity.java
+++ app/src/main/java/com/example/a1805toutiao/MainActivity.java
@@ -1,16 +1,87 @@
 package com.example.a1805toutiao;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.framework.base.BaseActivity;
+import com.example.framework.helper.BottomNavigationViewHelper;
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends BaseActivity {
+
+
+    @BindView(R.id.toolbar_left_iv)
+    ImageView toolbarLeftIv;
+    @BindView(R.id.toolbar_title)
+    TextView toolbarTitle;
+    @BindView(R.id.toolbar_right_iv)
+    ImageView toolbarRightIv;
+    @BindView(R.id.toolbar)
+    AppBarLayout toolbar;
+    @BindView(R.id.vp_main)
+    ViewPager viewpagerMain;
+    @BindView(R.id.bottom_main)
+    BottomNavigationView bottomMain;
+    @BindView(R.id.drawer_main)
+    DrawerLayout drawerMain;
+
+    @Override
+    protected void initPresenter() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void initData() {
+        toolbarTitle.setText("头条");
+    }
+
+    @Override
+    protected void initView() {
+        ButterKnife.bind(this);
+        toolbarLeftIv.setImageResource(R.drawable.list);
+        toolbarRightIv.setImageResource(R.drawable.search);
+        toolbar.setBackgroundResource(R.color.themeColor);
+        BottomNavigationViewHelper.disableShiftMode(bottomMain);
+    }
+
+    @Override
+    protected int getLayoutID() {
+        return R.layout.activity_main;
+    }
+
+
+    @OnClick({R.id.toolbar_left_iv, R.id.toolbar_right_iv})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.toolbar_left_iv:
+                break;
+            case R.id.toolbar_right_iv:
+                break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
     }
 }
---
+++ framework/src/main/java/com/example/framework/helper/BottomNavigationViewHelper.java
@@ -0,0 +1,26 @@
+package com.example.framework.helper;
+
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.lang.reflect.Field;
+
+public class BottomNavigationViewHelper {
+    public static void disableShiftMode(BottomNavigationView view){
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
+        try {
+            Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+            shiftingMode.setAccessible(true);
+            shiftingMode.setBoolean(menuView,false);
+            shiftingMode.setAccessible(false);
+
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+}
