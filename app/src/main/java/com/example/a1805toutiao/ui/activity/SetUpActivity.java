a9ce74b7d85a135e47c7c4449464e361a59fc092
Your <you@example.com>
2021-01-08 19:32:47 +0800

    设置

--- app/src/main/java/com/example/a1805toutiao/ui/activity/SetUpActivity.java
+++ app/src/main/java/com/example/a1805toutiao/ui/activity/SetUpActivity.java
@@ -34,7 +34,7 @@ public class SetUpActivity extends BaseActivity<IPresenter, IView>{
         //初始化控件
         frameSettingLayout = findViewById(R.id.frame_setting_layout);
         toolbar = findViewById(R.id.toolbar);
-
+        //显示出来
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.add(R.id.frame_setting_layout,preSettingFragment).commit();
